0d1cbf9f480f7edb67b594115c2d34557fbb6b5f
倪俊杰 <1006312908@qq.com>
2019-12-06 16:45:08 +0800

    暴露出stop方法用于nodemon的关闭 & 优化代码

--- demo/end/server.js
+++ demo/end/server.js
@@ -6,7 +6,7 @@ const KoaMockSwitch = require('../../index.js')
 // mock管理列表
 const mockSwitchMap = require('./mockSwitchMap.js')
 /**
- * KoaMockSwitch(mockRoot, mockSwitchMap, apiSuffix)
+ * config
  * @param mockRoot mock文件的根目录
  * @param mockSwitchMap mock管理列表
  * @param apiSuffix 客户端请求api的后缀，比如'/api/kitty.json'，apiSuffix就是'.json'
@@ -18,3 +18,11 @@ const mock = new KoaMockSwitch({
 })
 // 启动mock服务
 mock.start(7878)
+
+// nodemon会command+c终止终端都无法关闭mock进程
+// 所以需要进程接受信号来调用koa-mock-switch接口来关闭
+;['SIGINT', 'SIGTERM'].forEach(signal => {
+  process.on(signal, () => {
+    mock.stop()
+  })
+})
--- index.js
+++ index.js
@@ -27,80 +27,77 @@ class KoaMockSwitch {
   }
 
   start(port = 7777) {
-    // 使用mockSwitch
-    app.use(async (ctx, next) => {
-      if (ctx.path.startsWith('/mock-switch/list')) {
-        // '/mock-switch/list'是为了让接口管理页面'/mock-switch/'通过配置文件展现数据
-        ctx.body = this.mockSwitchMap
-      } else if (ctx.path.startsWith('/mock-switch/')) {
-        // '/mock-switch/'是接口管理页面
-        var fileName = ctx.path.substr('/mock-switch/'.length)
-        await send(
-          ctx,
-          './mockSwitch/' + (fileName || 'index.html'),
-          { root: __dirname + '/' }
-        )
-      } else if (ctx.path.startsWith('/mock-switch')) {
-        // '/mock-switch'是接口管理页面切换接口时候post的地址
-        const path = ctx.request.body.key
-        const value = ctx.request.body.value
-        const mockHandle = require(`${this.mockRoot}${path}.js`)
-        // ====
-        this.$cache[path] = processMock(mockHandle(), value)
-        // ====
-        ctx.body = this.$cache[path]
-      }
-      await next()
-    })
-
-    // 模拟数据返回
-    app.use(async (ctx, next) => {
-      if (!ctx.path.startsWith('/mock-switch') &&
-      ctx.path !== '/' &&
-      ctx.path !== '/favicon.ico' &&
-      ctx.path.indexOf('hot-update.json') === -1
-      ) {
-        // 模拟
-        let path = ctx.path.replace(this.apiSuffix, '')
-        // 调用对应的模拟数据
-        let mockHandle = require(`${this.mockRoot}${path}.js`)
-        // 返回数据
-        // 如果mock-switch设置过，则从cache中（即$cache）获取即可
-        if (this.$cache.hasOwnProperty(path)) {
-          ctx.body = this.$cache[path]
-        } else {
-          // 因为mock-switch对应的mock数据的数据结构的特殊性，需要设置一个默认值
-          // 因此遍历mockSwitchMap，如果当前path在mockSwitchMap中
-          // 则直接用第一项为默认值
-          const switchData = this.mockSwitchMap.find(item => item.url === path)
-          if (switchData) {
-            this.$cache[path] = processMock(mockHandle(), switchData.selections[0].value)
-            ctx.body = this.$cache[path]
-          } else {
-            // 不用mock-switch，正常返回
-            ctx.body = mockHandle()
-          }
-        }
-        await next()
-      }
-    })
-
-    // log error
+    // 通过cache来控制模拟数据的状态切换
+    app.use(this._mockDataByCacheMiddware.bind(this))
+    // 通过文件来模拟数据
+    app.use(this._mockDataByFileMiddware.bind(this))
+    // 错误打印
     app.on('error', (err, ctx) => {
       console.log('server error', err, ctx)
     })
-
     // 注意：这里的端口要和webpack里devServer的端口对应
-    const _server = app.listen(port)
+    this._server = app.listen(port)
+  }
 
-    // 关闭监听保证进程关闭
-    ;['SIGINT', 'SIGTERM'].forEach(signal => {
-      process.on(signal, () => {
-        _server.close(() => {
-          process.exit(0)
-        })
-      })
+  stop() {
+    this._server && this._server.close(() => {
+      process.exit(0)
     })
+  }
+
+  async _mockDataByCacheMiddware(ctx, next) {
+    // '/mock-switch/list' 是为了让接口管理页面'/mock-switch/'通过配置文件展现数据
+    // '/mock-switch/'     是接口管理页面
+    // '/mock-switch'      是接口管理页面切换接口时候post的地址
+    if (ctx.path.startsWith('/mock-switch/list')) {
+      ctx.body = this.mockSwitchMap
+    } else if (ctx.path.startsWith('/mock-switch/')) {
+      var fileName = ctx.path.substr('/mock-switch/'.length)
+      await send(
+        ctx,
+        './mockSwitch/' + (fileName || 'index.html'),
+        { root: __dirname + '/' }
+      )
+    } else if (ctx.path.startsWith('/mock-switch')) {
+      const path = ctx.request.body.key
+      const value = ctx.request.body.value
+      const mockHandle = require(`${this.mockRoot}${path}.js`)
+      // 模拟数据大管家 - cache
+      this.$cache[path] = processMock(mockHandle(), value)
+      ctx.body = this.$cache[path]
+    }
+    await next()
+  }
+
+  async _mockDataByFileMiddware(ctx, next) {
+    if (!ctx.path.startsWith('/mock-switch') &&
+      ctx.path !== '/' &&
+      ctx.path !== '/favicon.ico' &&
+      ctx.path.indexOf('hot-update.json') === -1
+    ) {
+      // 模拟
+      let path = ctx.path.replace(this.apiSuffix, '')
+      // 调用对应的模拟数据
+      let mockHandle = require(`${this.mockRoot}${path}.js`)
+      // 返回数据
+      // 如果mock-switch设置过，则从cache中（即$cache）获取即可
+      if (this.$cache.hasOwnProperty(path)) {
+        ctx.body = this.$cache[path]
+      } else {
+        // 因为mock-switch对应的mock数据的数据结构的特殊性，需要设置一个默认值
+        // 因此遍历mockSwitchMap，如果当前path在mockSwitchMap中
+        // 则直接用第一项为默认值
+        const switchData = this.mockSwitchMap.find(item => item.url === path)
+        if (switchData) {
+          this.$cache[path] = processMock(mockHandle(), switchData.selections[0].value)
+          ctx.body = this.$cache[path]
+        } else {
+          // 不用mock-switch，正常返回
+          ctx.body = mockHandle()
+        }
+      }
+      await next()
+    }
   }
 }
 
